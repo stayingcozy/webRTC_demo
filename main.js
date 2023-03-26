1e0e02c363afb6aad5f7cac0bdab2d0252e81aa6
stayingcozy <cody.colclough@gmail.com>
2023-03-26 09:46:16 -0500

    anoth pi cam read attempt

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ webcamButton.onclick = async () => {
   // localStream = await navigator.mediaDevices.mozGetUserMedia({ video: true, audio: true });
   // localStream = await navigator.mediaDevices.msGetUserMedia({ video: true, audio: true });
   // window.navigator.mediaDevices.getUserMedia({video: true}).then((stream => console.log(stream)));
-  localStream = await window.navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+  localStream = navigator.mediaDevices.getUserMedia({ video: true, audio: true });
   console.log('after local stream grab');
 
   remoteStream = new MediaStream();
